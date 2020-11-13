ade19a0c6ec26011e0315474dabd8346af175281
Julian David Ortiz Alviar <61361287+Jdavid0610@users.noreply.github.com>
2020-11-12 20:09:52 -0500

    inputs en hwg

--- src/app/services/conexion/conexion.service.ts
+++ src/app/services/conexion/conexion.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConexionService {
 
-  constructor() { }
+  constructor(router : Router,) {
+    
+   }
 }
